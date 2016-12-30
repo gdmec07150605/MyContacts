52a8ee124d0b6920409ab84d9f311e3151b3fec0
unknown <782230913@qq.com>
2016-12-30 14:59:09 +0800

    Initial commit

---
+++ app/src/main/java/gdmec07150605/lenovo/mycontacts/MainActivity.java
@@ -0,0 +1,13 @@
+package gdmec07150605.lenovo.mycontacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
